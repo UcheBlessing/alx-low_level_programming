890aa95cca94f3b1223fbb95455e516ddf9c600b
UcheBlessing <ucheblessing227@gmail.com>
2022-09-15 09:28:23 -0700

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,20 +1,52 @@
-
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
 #include <stdio.h>
 
-
-
-void positive_or_negative(int i);
-
-int largest_number(int a, int b, int c);
-
-void print_remaining_days(int month, int day, int year);
+#include "main.h"
 
 
 
-#endif /* MAIN_H */
+/**
+
+* main - takes a date and prints how many days are left in the year, taking
+
+* leap years into account
+
+* Return: 0
+
+*/
+
+
+
+int main(void)
+
+{
+
+    int month;
+
+    int day;
+
+    int year;
+
+
+
+    month = 4;
+
+    day = 01;
+
+    year = 1997;
+
+
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+
+
+    day = convert_day(month, day);
+
+
+
+    print_remaining_days(month, day, year);
+
+
+
+    return (0);
+
