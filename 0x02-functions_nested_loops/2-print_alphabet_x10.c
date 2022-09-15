b4cc6384aba5b8ea276afb218d63f2e77aebc560
UcheBlessing <ucheblessing227@gmail.com>
2022-09-14 17:34:58 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,45 +1,35 @@
-nclude "main.h"
+#include "main.h"
 
 
 
 /**
  *
- *  * print_alphabet_x10 - prints alphabet 10 times
+ *  * print_alphabet - function to print abc
  *
- *   *
+ *   * 
  *
- *    * Return: always 0;
+ *    * Return: always 0
  *
- *     *
- *
- *      */
+ *     */
 
 
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 
-{
+{ 
 
-		char i;
+	      char c;
 
-			char h;
+	            for (c = ‘a’; c <= ‘z’; c++)
 
+		    {
 
+			    	_putchar(c);
 
-				for (h = 0; h <= 9; h++)
+		    }
 
-						{
-
-									for (i = 'a'; i <= 'z'; i++)
-
-												{
-
-																_putchar(i);
-
-																		}
-
-											_putchar('\n');
-
-												}
+		    _putchar(‘\n’);
 
 }
+
+
