1e3c70dcd582eba719a09bcd4ad5c32a706f1d77
UcheBlessing <ucheblessing227@gmail.com>
2022-09-22 16:56:33 -0700

    Another commit on pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,32 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _indexOf - returns boolean if special  character
- * @a: character to return
- * Return: true or false
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ * Return: the resulting string
  */
 
-int _indexOf(char a)
-{
-	int i;
-	char capArr[13] = {'\n', '\t', ' ', '.', ',', ';', ',', '!', '?', '(',
-')', '{', '}'};
-
-	for (i = 0; i < 13; i++)
-	{
-		if (capArr[i] == a)
-			return (1);
-	}
-	return (0);
-}
-/**
- * cap_string - capitalizes the string
- * @s: string
- * Return: the string capitalized
- */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 
+		'!', '?', '"', '(', ')', '{', '}'};
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (_indexOf(s[i]))
-			continue;
-		if (s[i] >= 'a' && s[i] <= 'z' && (_indexOf(s[i - 1]) || i == 0))
-			s[i] = s[i] - 32;
-
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
+
 	return (s);
 }
